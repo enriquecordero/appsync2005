4807eb1b675fdd02f4863fea223a1eace7306629
Enrique Cordero <ecs5000@gmail.com>
2025-11-24 16:47:49 -0400

    adding unittest

--- test/appsync2025.test.ts
+++ test/appsync2025.test.ts
@@ -1,17 +1,54 @@
-// import * as cdk from 'aws-cdk-lib';
-// import { Template } from 'aws-cdk-lib/assertions';
-// import * as Appsync2025 from '../lib/appsync2025-stack';
+import * as cdk from "aws-cdk-lib";
+import { Template, Match } from "aws-cdk-lib/assertions";
+import * as Appsync2025 from "../lib/appsync2025-stack";
 
-// example test. To run these tests, uncomment this file along with the
-// example resource in lib/appsync2025-stack.ts
-test('SQS Queue Created', () => {
-//   const app = new cdk.App();
-//     // WHEN
-//   const stack = new Appsync2025.Appsync2025Stack(app, 'MyTestStack');
-//     // THEN
-//   const template = Template.fromStack(stack);
+test("AppSync Stack Created", () => {
+    const app = new cdk.App();
+    // WHEN
+    const stack = new Appsync2025.Appsync2025Stack(app, "MyTestStack");
+    // THEN
+    const template = Template.fromStack(stack);
 
-//   template.hasResourceProperties('AWS::SQS::Queue', {
-//     VisibilityTimeout: 300
-//   });
+    // Verify AppSync API created
+    template.hasResourceProperties("AWS::AppSync::GraphQLApi", {
+        Name: "cdk-appsync-api",
+        AuthenticationType: "API_KEY",
+    });
+
+    // Verify DynamoDB Table created
+    template.hasResourceProperties("AWS::DynamoDB::Table", {
+        TableName: "Books",
+        BillingMode: "PAY_PER_REQUEST",
+        KeySchema: [
+            {
+                AttributeName: "id",
+                KeyType: "HASH",
+            },
+        ],
+    });
+
+    // Verify Lambda Function created
+    template.hasResourceProperties("AWS::Lambda::Function", {
+        Runtime: "nodejs20.x",
+        Environment: {
+            Variables: {
+                NODE_ENV: "production",
+                BOOKS_TABLE: {
+                    Ref: Match.stringLikeRegexp("BooksTable.*"),
+                },
+            },
+        },
+    });
+
+    // Verify Data Source created
+    template.hasResourceProperties("AWS::AppSync::DataSource", {
+        Type: "AWS_LAMBDA",
+        Name: "ListBooksLambdaDataSource",
+    });
+
+    // Verify Resolver created
+    template.hasResourceProperties("AWS::AppSync::Resolver", {
+        TypeName: "Query",
+        FieldName: "ListBooks",
+    });
 });
